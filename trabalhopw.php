41ca561e7786cd0559d8634c26650815fbec1036
BrunoPW1 <90937698+BrunoPW1@users.noreply.github.com>
2021-10-08 17:59:46 -0300

    Add files via upload

---
+++ trabalhopw.php
@@ -0,0 +1,44 @@
+<?php
+require_once './vendor/autoload.php';
+
+use TrabalhoPDOMySQL\MySQLConnection;
+
+$bd = new MySQLConnection();
+
+$genero = null;
+
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $comando = $bd->prepare('SELECT * FROM genero WHERE id = :id');
+    $comando->execute([':id' => $_GET['id']]);
+
+    $genero = $comando->fetch(PDO::FETCH_ASSOC);
+
+} else {
+    $comando = $bd->prepare('DELETE FROM generos WHERE id = :id WHERE id = :id');
+    $comando->execute([':id'=> $_POST['id']]);
+
+    header('Location:/index.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>Remover Gênero</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container">
+    <h1>Remover Gênero</h1>
+    <p>Tem certeza que deseja remover este gênero <?= $genero['nome'] ?> ?</p>
+
+    <form action="delete.php" method="post">
+        <input type="hidden" name="id" value="<?= $genero['id'] ?>" />
+        <a class="btn btn-secondary" href="index.php">Voltar</a>
+        <button class="btn btn-danger" type="submit">Excluir</button>
+        </form>
+</main>
+    </body>
+</html>
